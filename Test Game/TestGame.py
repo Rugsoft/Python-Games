452686ff7f5abb2678d56f59d43313108aa6eec0
Rugsoft <futuroelectronico@gmail.com>
2025-10-05 10:09:44 +0200

    clock añadido

--- Test Game/TestGame.py
+++ Test Game/TestGame.py
@@ -1,3 +1,21 @@
 import pygame
 from sys import exit
 
+pygame.init()
+
+width = 1280
+height = 720
+fps = 60
+
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Test Game")
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    
+    pygame.display.update()
+    clock.tick(fps)
