5185a9335cd04d2a2fec92a75403cd6b8e96ac09
Rugsoft <futuroelectronico@gmail.com>
2025-10-05 20:28:09 +0200

    Añadido rectangulo al screen

--- Test Game/TestGame.py
+++ Test Game/TestGame.py
@@ -11,11 +11,16 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Test Game")
 clock = pygame.time.Clock()
 
+test_surface = pygame.Surface((200,100))
+test_surface.fill("yellow")
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+            
+    screen.blit(test_surface, (720,350))
     
     pygame.display.update()
     clock.tick(fps)
