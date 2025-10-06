69a46ee84c5aeae80ce3f933e0ff2de0c79eb372
Rugsoft <futuroelectronico@gmail.com>
2025-10-06 09:27:22 +0200

    Añadido titulo

--- Test Game/TestGame.py
+++ Test Game/TestGame.py
@@ -10,9 +10,11 @@ fps = 60
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Test Game")
 clock = pygame.time.Clock()
+test_font = pygame.font.Font(None, 50)
 
 sky_surface = pygame.image.load("graphics/Sky.jpg")
 ground_surface = pygame.image.load("graphics/Ground.png")
+text_surface = test_font.render("Rugsoft", False, "Green")
 
 while True:
     for event in pygame.event.get():
@@ -22,6 +24,7 @@ while True:
       
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0, 575))   
+    screen.blit(text_surface, (600, 50))
     
     pygame.display.update()
     clock.tick(fps)
